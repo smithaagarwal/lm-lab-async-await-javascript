d58d66244f2ebb09b16a774a38dbe264090ad3b5
Smitha <smitha.kotian@gmail.com>
2023-11-04 20:04:47 +0000

    feat: refactor exercise1 to use await async

--- exercise001.js
+++ exercise001.js
@@ -1,21 +1,36 @@
-const promise = new Promise((resolve, reject) => {
-  // Generate random number between 0 and 9
-  const randomInt = Math.floor(Math.random() * 10);
+const generateRandomEvenNumber = () => {
+  return new Promise((resolve, reject) => {
+    // Generate random number between 0 and 9
+    const randomInt = Math.floor(Math.random() * 10);
 
-  if (randomInt % 2 === 0) {
-    // Success
-    setTimeout(() => resolve("done"), 2000);
-  } else {
-    // Failure
-    setTimeout(() => reject("error"), 2000);
-  }
-});
+    if (randomInt % 2 === 0) {
+      // Success
+      setTimeout(() => resolve("done"), 2000);
+    } else {
+      // Failure
+      setTimeout(() => reject("error"), 2000);
+    }
+  });
+};
 
 // Your solution(s) to exercise001 here!
-promise
-  .then((response) => {
-    console.log(`Yay! Promise resolved with response: ${response}`);
-  })
-  .catch((response) => {
-    console.log(`Boo. Promise rejected with response: ${response}`);
-  });
+// promise
+//   .then((response) => {
+//     console.log(`Yay! Promise resolved with response: ${response}`);
+//   })
+//   .catch((response) => {
+//     console.log(`Boo. Promise rejected with response: ${response}`);
+//   });
+
+const getEvenRandomNumber = async () => {
+  try {
+    const hasEvenNumberGenerated = await generateRandomEvenNumber();
+    console.log(
+      `Yay! Promise resolved with response: ${hasEvenNumberGenerated}`
+    );
+  } catch (error) {
+    console.log(`Boo. Promise rejected with response: ${error}`);
+  }
+};
+
+getEvenRandomNumber();
